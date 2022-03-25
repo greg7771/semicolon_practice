8e055ce6022eec573eeedd45f5275243ac1c7e7d
Odyssey <63042773+greg7771@users.noreply.github.com>
2022-03-25 08:10:40 -0400

    Semicolon_Examples
    First commit

---
+++ calculation_practice.py
@@ -0,0 +1,5 @@
+print(1+1)
+print(1-1)
+print(12/3)
+print(12*3)
+print(12**3) #제곱
---
+++ if_practice.py
@@ -0,0 +1,16 @@
+animal = input("어떤 동물을 조아하나여? ")
+
+if animal == "고양이" :
+    print("역시 고양이가 제일 귀엽죠?")
+
+else:
+    print(f"{animal}도 귀엽지요?")
+
+
+temp = int(input("오늘의 몇도 인가요?"))
+
+if temp == 20 :
+    print("오늘의 동물은 비둘기 입니다!")
+
+else:
+        print("그러면 고양이가 제일 낫겠네요!")
