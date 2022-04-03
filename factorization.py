3702d2712a92add39b8fa8baf75387d4c64dd1aa
Odyssey <63042773+greg7771@users.noreply.github.com>
2022-04-03 10:55:55 -0400

    Half-Completed Factorization

--- factorization.py
+++ factorization.py
@@ -1,10 +1,39 @@
+import math
 a = int(input("최고차항의 계수 > "))
 b = int(input("x의 계수 > "))
 c = int(input("상수항 > "))
 
+root0 = (-b + math.sqrt(b**2 -4 * a * c)) / (2*a)
 
-def f(x):
-    return a * (x **2) + b * x + c
+root1 = (-b - math.sqrt(b**2 -4 * a * c)) / (2*a)
 
-def factorization():
-    -b
+# def f(x):
+#     a*(x**2) + b*x + c
+
+result = f"(x{int(root0)})(x{int(root1)})"
+
+if root0 == root1 and root0 > 0:
+    result = f"(x-{int(root0)})^2"
+
+elif root0 == root1 and root0 < 0:
+    root0 = -1*root0
+    result = f"(x+{int(root0)})^2"
+
+elif root0 > 0 and root1 < 0:
+    root1 = -1*root1
+    result = f"(x-{int(root0)})(x+{int(root1)})"
+
+elif root0 < 0 and root1 > 0:
+    root0 = -1*root0
+    result = f"(x+{int(root0)})(x-{int(root1)})"
+
+elif root0 > 0 and root1 > 0:
+    result = f"(x-{int(root0)})(x-{int(root1)})"
+
+elif root0 < 0 and root1 < 0:
+    root0 = -1*root0
+    root1 = -1*root1
+    result = f"(x+{int(root0)})(x+{int(root1)})"
+
+print()
+print(result)
