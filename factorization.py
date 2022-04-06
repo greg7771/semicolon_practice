ae04714404c9af510bf043351f28dea37aa3aa3f
Odyssey <63042773+greg7771@users.noreply.github.com>
2022-04-06 00:34:05 -0400

    3 out of 4 ready

--- factorization.py
+++ factorization.py
@@ -1,4 +1,7 @@
 import math
+print()
+print("실수범위의 인수분해를 해주는 프로그램 입니다.")
+print()
 a = int(input("최고차항의 계수 > "))
 b = int(input("x의 계수 > "))
 c = int(input("상수항 > "))
@@ -7,33 +10,30 @@ root0 = (-b + math.sqrt(b**2 -4 * a * c)) / (2*a)
 
 root1 = (-b - math.sqrt(b**2 -4 * a * c)) / (2*a)
 
-# def f(x):
-#     a*(x**2) + b*x + c
-
 result = f"(x{int(root0)})(x{int(root1)})"
 
 if root0 == root1 and root0 > 0:
-    result = f"(x-{int(root0)})^2"
+    result = f"(x-{float(root0)})^2"
 
 elif root0 == root1 and root0 < 0:
     root0 = -1*root0
-    result = f"(x+{int(root0)})^2"
+    result = f"(x+{float(root0)})^2"
 
 elif root0 > 0 and root1 < 0:
     root1 = -1*root1
-    result = f"(x-{int(root0)})(x+{int(root1)})"
+    result = f"(x-{float(root0)})(x+{float(root1)})"
 
 elif root0 < 0 and root1 > 0:
     root0 = -1*root0
-    result = f"(x+{int(root0)})(x-{int(root1)})"
+    result = f"(x+{float(root0)})(x-{float(root1)})"
 
 elif root0 > 0 and root1 > 0:
-    result = f"(x-{int(root0)})(x-{int(root1)})"
+    result = f"(x-{float(root0)})(x-{float(root1)})"
 
 elif root0 < 0 and root1 < 0:
     root0 = -1*root0
     root1 = -1*root1
-    result = f"(x+{int(root0)})(x+{int(root1)})"
+    result = f"(x+{float(root0)})(x+{float(root1)})"
 
 print()
 print(result)
