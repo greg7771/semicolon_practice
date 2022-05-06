fd2b6b1f6e3478e4ec64cd25ccdcb4fbb9839a7c
Odyssey <63042773+greg7771@users.noreply.github.com>
2022-05-06 20:51:25 +0900

    Update factorization.py

--- factorization.py
+++ factorization.py
@@ -7,7 +7,7 @@ while True:
     a = input("\nx^2의 계수를 입력하세요 > ")
     try:
         a = int(a)
-    except ValueError: # a 가 정수가 아니면 Valueerror 발생 그러면 정수로 입력하라고 다시 안내
+    except ValueError: # a 가 정수가 아니면 ValueError 발생 그러면 정수로 입력하라고 다시 안내
         print("\nx^2의 계수는 0이 아닌 정수로 입력하세요.")
     else:
         break
@@ -48,7 +48,7 @@ result = f"(x{int(root0)})(x{int(root1)})"
 
 if root0 == root1 and root0 > 0:
     if type(root0) is int and type(root1) is int:
-        result == f"(x-{int(root0)})^2"
+        result = f"(x-{int(root0)})^2"
     result = f"(x-{int(root0)})^2"
 
 elif root0 == root1 and root0 < 0:
@@ -66,12 +66,29 @@ elif root0 < 0 and root1 > 0:
     root0 = -1*root0
     result = f"(x+{float(root0)})(x-{float(root1)})"
 
-elif root0 > 0 and root1 > 0:
+elif root0 > 0 and root1 > 0: #2번 근이 모두 음수일때
     result = f"(x-{float(root0)})(x-{float(root1)})"
 
-elif root0 < 0 and root1 < 0:
+elif root0 < 0 and root1 < 0: # 1번 근이 모두 양수일때
     root0 = -1*root0
     root1 = -1*root1
     result = f"(x+{float(root0)})(x+{float(root1)})"
+
+if a > 1 and root0 > 0 and root1 > 0: # 1번 근이 모두 양수
+    if type(int(root0)) is int and type(int(root1)) is int:
+        pass
+    else:
+        root0 = a * root0
+        root1 = a * root1
+    result = f"{a}(x+{float(root0)})(x+{float(root1)})"
+    if type(int(root0)) is int and type(int(root1)) is int:
+        result = f"{a}(x+{int(root0)})(x+{int(root1)})"
+    else:
+        result = f"{a}(x+{float(root0)})(x+{float(root1)})"
+
+if a > 1 and root0 < 0 and root1 < 0: # 2번 근이 모두 음수
+    result = f"{a}(x-{float(root0)})(x-{float(root1)})"
+    if type(int(root0)) is int and type(int(root1)) is int:
+        result = f"{a}(x-{int(root0)})(x-{int(root1)})"
 
 print(f"\n계산결과는 > {result} 입니다!")
